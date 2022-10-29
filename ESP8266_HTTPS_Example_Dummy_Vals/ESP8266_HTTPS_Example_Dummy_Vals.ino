6dd462068607b64332b631ff81515525cf202a0f
nathanfield1990 <52389004+nathanfield1990@users.noreply.github.com>
2022-10-29 10:19:25 -0300

    Update ESP8266_HTTPS_Example_Dummy_Vals.ino

--- ESP8266_HTTPS_Example_Dummy_Vals/ESP8266_HTTPS_Example_Dummy_Vals.ino
+++ ESP8266_HTTPS_Example_Dummy_Vals/ESP8266_HTTPS_Example_Dummy_Vals.ino
@@ -6,7 +6,7 @@
  You can also find your own IP and get all the information, find yours here and sub in your IP below
  https://www.whatismyip.com/
  
- This sketch is built and tested on a ESP8266
+ This sketch is built and tested on an ESP8266
  
  */
 
@@ -18,7 +18,7 @@
 #include <WiFiClientSecureBearSSL.h>
 
 ESP8266WiFiMulti WiFiMulti;
-// Fingerprint for demo URL, expires on June 2, 2021, needs to be updated well before this date
+// Fingerprint for URL (not being used currently)
 // const uint8_t fingerprint[20] = {0x40, 0xaf, 0x00, 0x6b, 0xec, 0x90, 0x22, 0x41, 0x8e, 0xa3, 0xad, 0xfa, 0x1a, 0xe8, 0x25, 0x41, 0x1d, 0x1a, 0x54, 0xb3};
 
 // Variable decleration
@@ -53,9 +53,9 @@ void loop() {
 
     std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
 
-    // Note, not using SSL in this example, if you were, you would uncomment below and comment the setInsecure line below
-    //client->setFingerprint(fingerprint);
-    //Or, if you happy to ignore the SSL certificate, then use the following line instead:
+    // Note, not using SSL in this example, if you were, you would uncomment below and comment the setInsecure line below and use the fingerprint 
+    // client->setFingerprint(fingerprint);
+    // Or, if you happy to ignore the SSL certificate, then use the following line instead:
     client->setInsecure();
 
     HTTPClient https;
@@ -64,7 +64,7 @@ void loop() {
     
     //if (https.begin(*client, "https://ipapi.co/8.8.8.8/json/")) {  // Public URL that can be used, to use your own comment this out and uncomment below
     
-    if (https.begin(*client, "https://ipapi.co/" + yourIP + "/json/")) {  // Sub in your IP here and comment above (set variable in variable decleration at the top)
+    if (https.begin(*client, "https://ipapi.co/" + yourIP + "/json/")) {  // Uses your public IP address (set in variable decleration) - to use the public IP comment this out and uncomment above
       
       Serial.print("[HTTPS] GET...\n");
       // start connection and send HTTP header
